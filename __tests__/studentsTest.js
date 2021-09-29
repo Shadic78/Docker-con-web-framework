87aff9caf31c9b2b54d502131a6ab518ef6b4d59
Shadic78 <carlosjosue7889@gmail.com>
2021-09-28 19:34:25 -0500

    Bad test

--- __tests__/studentsTest.js
+++ __tests__/studentsTest.js
@@ -5,6 +5,6 @@ describe('Student endpoint test', () => {
   it('Should return 6 students from /students', async () => {
     const response = await request(app).get('/students');
     expect(response.statusCode).toEqual(200);
-    expect(response.body).toHaveLength(6);
+    expect(response.body).toHaveLength(5);
   });
 });
